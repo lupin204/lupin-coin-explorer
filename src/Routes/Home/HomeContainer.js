1d952d690024d22a58fb06de989d8e544a675f52
Lee Young Seo <lupin204@gmail.com>
2018-07-18 15:35:05 +0900

    move "Routes" folder

---
+++ src/Routes/Home/HomeContainer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+
+import HomePresenter from "./HomePresenter";
+
+class HomeContainer extends Component {
+    render() {
+        return <HomePresenter {...this.props} />;
+    }
+}
+
+HomeContainer.propTypes = {
+    blocks: PropTypes.array.isRequired,
+    transactions: PropTypes.array.isRequired
+};
+
+export default HomeContainer;
---
+++ src/Routes/Home/HomePresenter.js
@@ -0,0 +1,52 @@
+import React, { Fragment } from "react";
+import styled from "styled-components";
+import PropTypes from "prop-types";
+import _ from "lodash";
+
+import { makeDateSexy } from "../../utils";
+import { Card, BlocksHeader, BlocksRow, TxHeader, TxRow } from "Components/Shared";
+
+const TableContainer = styled.div`
+  margin-top: 50px;
+  margin-bottom: 100px;
+`;
+
+const HomePresenter = ({ blocks, transactions }) => (
+  <Fragment>
+    <TableContainer>
+      <h2>Latest Blocks</h2>
+      <Card>
+        <BlocksHeader />
+        {blocks.map(block => (
+          <BlocksRow
+            index={block.index}
+            hash={block.hash}
+            timestamp={makeDateSexy(block.timestamp)}
+            difficulty={block.difficulty}
+          />
+        ))}
+      </Card>
+    </TableContainer>
+    <TableContainer>
+      <h2>Latest Transactions</h2>
+      <Card>
+        <TxHeader />
+        {transactions.map(transaction => (
+          <TxRow
+            timestamp={makeDateSexy(transaction.timestamp)}
+            id={transaction.id}
+            insOuts={`${transaction.txIns.length}/${transaction.txOuts.length}`}
+            amount={_(transaction.txOuts.map(txOut => txOut.amount)).sum()}
+          />
+        ))}
+      </Card>
+    </TableContainer>
+  </Fragment>
+);
+
+HomePresenter.propTypes = {
+  blocks: PropTypes.array.isRequired,
+  transactions: PropTypes.array.isRequired
+};
+
+export default HomePresenter;
